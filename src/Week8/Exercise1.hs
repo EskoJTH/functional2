b7f0a03183af051f7bd6a3ccbe9c8c58b182f45b
esko <esko@pop-os.localdomain>
2020-01-02 06:21:51 +0200

    Deriving and stuff

--- src/Week8/Exercise1.hs
+++ src/Week8/Exercise1.hs
@@ -1,5 +1,8 @@
 module Week8.Exercise1 where
-
+import Data.Stream.Infinite (Stream (..))
+import qualified Data.Tree as Rose (Forest (..), Tree (..))
+import qualified Data.Tree.Binary.Inorder as Binary (Tree (..))
+import Data.List.NonEmpty
 ----    Derivative of Maybe a.
 {-
 
@@ -41,7 +44,7 @@ helper definition for list:
 ≅ 1 + x × (1 + x × (1 + x × ...))
 ≅ 1 + x + x^2 + x^3 × ...
 
-F := (1 + y × x)
+F ≡ (1 + y × x)
   D_x (μy(1 + y × x))
 ≅ μz( (λy(D_x(1 + y × x))(μy.F)  +  (λy(D_y(1 + y × x))(μy.F)  × z)
 ≅ μz( (λy(y)(μy.F)  +  (λy(x)(μy.F)  × z)
@@ -58,9 +61,8 @@ F := (1 + y × x)
 ≅ (1 + x + x^2 × x^3 + ...) × (1 + x + x^2 + x^3 + ...)
 ≅ [x] × [x]
 
-Omitted writing descriptions for operations.
-
 -}
+-- data DSA a = DSA1 [a] | DSA2 a (DSA a)
 
 data ListContext a = LC ([a],[a])
 data ListZipper a = LZ (a, ([a],[a]))
@@ -68,30 +70,294 @@ data ListZipper a = LZ (a, ([a],[a]))
 ----    Derivative of Stream a from the streams package.
 {-
 data Stream a = a :> Stream a
-μx (a,x)
-a×(a×(a×...))
-infinity
+μx (x,y)    -- x×(x×(x×...))
+derivative μx (x×y)
+F ≡ (x×y)
+≅ μz( (λy(D_x(y))(μy.F))  +  (λy(D_y(x))(μy.F)  × z))
+≅ μz( (μy.(x×y))  +  x × z)
+≅ (μy.(x×y))  +  x × μz( (μy.(x×y))  +  x × z)
+≅ x×(x×(x×...)) + x × μz( (x×(x×(x×...))) + x × z)
+≅ x×(x×(x×...)) + x × (x×(x×(x×...)) + x × μz( (x×(x×(x×...))) + x × z))
+≅ x×(x×(x×...)) + x × x×(x×(x×...)) + x*2 × μz( (x×(x×(x×...))) + x × z)
+≅ x×(x×(x×...)) + x × x×(x×(x×...)) + x*2 × (x×(x×(x×...))) + x^3 × μz((x×(x×(x×...))) + x × z)
+≅ x×(x×(x×...)) + (1 + x + x^2 + x^3 + ...)
+≅ μx (x×y) + μy(1 + y × x)
+≅ Stream x + [x]
 
-the answer is probably something like :
-[a]×Stream a
 -}
+
+data StreamContext a = SC ([a], Stream a)
+data StreamZipper a = SZ (a, ([a], Stream a))
 
 ----    Derivative of Tree a from the binary-tree package.
 {-
 
-btree = 1 + btree^2
-btr
-D btree ->  2 × btree
+btree = 1 + x × btree^2
+μy (1 + x × y^2)
+derivative:
+F ≡ (1 + x × y^2)
+≅ μz( (λy(D_x(1 + x × y^2))(μy.F))  +  (λy(D_y(1 + x × y^2))(μy.F)  × z))
+≅ μz( (λy(y^2)(μy.F))  +  (λy(2x × y)(μy.F) × z))
+≅ μz( μy(1 + x × y^2)^2  +  (2x × μy(1 + x × y^2)) × z)
 
-answer: (Bool, btree)
+
+≅ μz( μy(1 + x × y^2)^2  +  (2x × μy(1 + x × y^2)) × z)
+
+
+data BTC' a = BTCL' (Binary.Tree a) (Binary.Tree a) | BTCR' Two a (Binary.Tree a) (BTC' a)
+
+-}
+
+data Two = L | R
+
+data BTC a = (Two, a, (Binary.Tree a)) :-< (BTC a) | BTCEnd (Binary.Tree a) (Binary.Tree a)
+data BTZ a = BTZ (a, BTC a)
+
+{-
+data BTreeContext a = BTC [(Bool, Binary.Tree a)]
+data BTreeZipper a = BTZ (a, [(Bool, Binary.Tree a)])
+
+data BTreeContext a = BTC [(Bool, Binary.Tree a)]
+data BTreeZipper a = BTZ (a, [(Bool, Binary.Tree a)])
 -}
 
 
-----    Derivative of Tree a from the containers package.
+
+
+
+---- Derivative of Tree a from the containers package.
 {-
 
-btree = 1 + ttree^3
-D btree ->  3 × ttree^2
-answer: ((Bool|()), (ttree,ttree))
+RoseTree = (x, [RoseTree x])
+RoseTree = x × [RoseTree x]
+μy(x × μy'(1 + y × y'))
+
+F ≡ (x × μy'(1 + y × y'))
+F' ≡ (1 + y × y')
+
+derivative:
+-}
+{-
+  μz( λy(D_x(x × μy'(1 + y × y')))(μy.F)  +  λy(D_y(x × μy'(1 + y × y')))(μy.F) × z)
+≅ μz( λy(μy'(1 + y × y'))(μy.F)  +  λy(D_y(μy'(x + y × y')))(μy.F) × z)
+≅ μz( λy(μy'(1 + y × y'))(μy.F)  +  λy(μz' ((λy'(D_y(x + y × y')))(μy.F') + λy'(D_y'(x + y × y'))(μy.F') × z')(μy.F)) × z)
+≅ μz( λy(μy'(1 + y × y'))(μy.F)  +  λy(μz'(λy'(y'))(μy.F') + λy'(y)(μy.F') × z')(μy.F) × z)
+≅ μz( λy(μy'(1 + y × y'))(μy.F)  +  λy(μz'(μy'(1 + y × y') + y × z'))(μy.F) × z)
+-}
+
+type RoseContainer = Z
+type RoseZipper x = (x , Z x)
+
+data Z x = RTLZ [Rose.Tree x] | (RZ x) :* (Z x)
+
+-- λy(μy'(1 + y × y'))(μy.F)
+-- ≅ (μy'(1 + μy(x × μy'(1 + y × y')) × y'))
+--type LZ x = [LZ' x]
+-- μy(x × μy'(1 + y × y'))
+-- data LZ' x = LZ' (x, [LZ' x])
+-- type LZ' x = Rose.Tree x
+
+-- λy(μz'(μy'(1 + y × y') + y × z'))(μy.F)
+-- μz'(μy'(1 + (μy(x × μy'(1 + y × y'))) × y') + (μy(x × μy'(1 + y × y'))) × z')
+data RZ x = LRZ [Rose.Tree x] | (Rose.Tree x) :-: (RZ x)
+
+-- ([rose], [rose]) ?
+
+-- (μy(x × μy'(1 + y × y')))
+--data LRZ x = LRZ (x, [LRZ x])
+-- (μy(x × μy'(1 + y × y')))
+--type RRZ x = LRZ x
+
+
+
+
+
+{-
+≅ μz( λy(μy'(1 + y × y'))(μy.F)  +  μz'((1 + (x × μy'(1 + y × y')) × y') + (x × μy'(1 + y × y')) × z') × z)
+≅ μz( (μy'(1 + μy(x × μy'(1 + y × y')) × y'))  +  μz'((1 + μy(x × μy'(1 + y × y')) × y') + μy(x × μy'(1 + y × y')) × z') × z)
+≅ μz( (μy'(1 + μy(x × μy'(1 + y × y')) × y'))  +  μz'((1 + μy(x × μy'(1 + y × y')) × y') + μy(x × μy'(1 + y × y')) × z') × z)
+-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+----  Not related derivative of a Tree a not from the containers package...
+{-
+RoseTree = x + [RoseTree x]
+μy(x + μy'(1 + y × y'))
+F ≡ (x + μy'(1 + y × y')
+F' ≡ (1 + y × y')
+derivative:
+
+--  
+μz( λy(D_x(x + μy'(1 + y × y')))(μy.F)  +  λy(D_y(x + μy'(1 + y × y')))(μy.F) × z)
+μz( (1 + 0)    +  λy(0 + D_y μy'(1 + y × y'))(μy.F) × z)
+μz( 1  +  λy(μz'( λy'(D_y(1 + y × y'))(μy'.F') + λy'(D_y'(1 + y × y'))(μy'.F') × z'))(μy.F) × z)
+μz( 1  +  λy(μz'( λy'(y')(μy'.F')              + λy'(y)(μy'.F')                × z'))(μy.F) × z)
+μz( 1  +  λy(μz'( λy'(y')(μy'.F') + λy'(y)(μy'.F') × z'))(μy.F) × z)
+μz( 1  +  λy(μz'( μy'(1 + y × y') + y × z'))(μy.F) × z)
+μz( 1  +  (μz'( μy'(1 + (μy(x + μy'(1 + y × y'))) × y') + (μy(x + μy'(1 + y × y'))) × z')) × z)
+μz( 1  +  (μz'( μy'(1 + (μy(x + μy'(1 + y × y'))) × y') + (μy(x + μy'(1 + y × y'))) × z')) × z)
+
+-}
+
+{-
+data IZ x = StopIZ | IZ (IZ' x) (IZ x) --list containing Z' things
+data IZ' x = StopIZ' (IY' x) | IZ' (IY x) (IZ' x) -- list Ending in Y' and containing Y's
+data IY x = StopIY x | IY (IY' x) 
+data IY' x = StopIY' | IY' (IY x) (IY' x) -- list containing Y 
+
+type IIZ x = [(IIZ' x)] --list containing IIZ' things
+data IIZ' x = StopIIZ' [(IIY x)] | (IIY x) :<<<> (IIZ' x) -- list Ending in IIY' and containing IIY's
+data IIY x = StopIIY x | IIY [(IIY x)] 
+type IIY' x = [(IIY x)] -- list containing some sort of recursive product type
+
+type IIIZ x = [IIIZ' x] --list containing IIIZ' things
+type IIIZ' x = (IIIY' x, [IIIY x]) -- list Ending in IIIY' and containing IIIY's
+data IIIY x = IIIL' x | IIIR' (IIIY' x) -- there is a cycle... ._.
+type IIIY' x = [IIIY x] -- list containing some sort of recursive product type
+
+type IIIIZ x = [([IIIIY x], [IIIIY x])]
+data IIIIY x = IIIIL' x | IIIIR' [IIIIY x]
+
+type Z x = [([Y x], [Y x])]
+type Y x = (x :| [Y x])
+
+-}
+
+{-
+type Z x = [(Z' x)] 
+data Z' x = StopZ' [(Y x)] | (Y x) :<> (Z' x) -- list Ending in Y' and containing Y's
+data Y x = StopY x | Y [(Y x)] 
+
+data RoseContext x = RC (Z x)
+data RoseZipperr x = RZ (x, Z x)
+-}
+
+
+
+{-
+data Three = One | Two | Three
+data TTreeContext a = TTC [(Three, (Rose.Tree a, Rose.Tree a))]
+data TTreeZipper a =  TTZ (a, [(Three, (Rose.Tree a, Rose.Tree a))])
+-}
+{-
+
+μy(x + x y)
+x + x μy(x + x y)
+(x + x^2 + x^3 × ...)
+nonempty
+
+(x, [x])
+x × μy(1 + x y)
+
+(1 + x + x^2 + x^3 × ...)
+
+  -}
+
+
+{-
+
+------ Derivative of Tree a from the binary-tree package.
+------ Being lost:
+------ 
+------ helper definition for list:
+------   [a]
+------ ≅ (μy(1 + x × y))
+------ ≅ (1 + x × (μy(1 + x × y)))
+------ ≅ (1 + x × (1 + x × (μy(1 + x × y))))
+------ ≅ (1 + x × (1 + x × (1 + x × (μy(1 + x × y)))))
+------ ≅ 1 + x × (1 + x × (1 + x × ...))
+------ ≅ 1 + x + x^2 + x^3 × ...
+------ ≅ Σ_n∈N x^n
+------ 
+------ 
+------ helper definition for a btree:
+------   btree a
+------ ≅ μy (1 + x × y^2)
+------ ≅ 1 + x × μy (1 + x × y^2)^2
+------ ≅ 1 + x × μy (1 + x × y^2)^2
+------ ≅ 1 + x × (1 + x × μy (1 + x × y^2)^2)^2
+------ ≅ 1 + x × (1 + x × (1 + x × μy (1 + x × y^2)^2)^2)^2
+------ ≅ 1 + x × (1 + 2x × (1 + x × μy (1 + x × y^2)^2)^2   + (x × (1 + x × μy (1 + x × y^2)^2)^2)^2)
+------ ≅ 1 + x + 2x^2 × (1 + x × μy (1 + x × y^2)^2)^2      +  x(x × (1 + x × μy (1 + x × y^2)^2)^2)^2
+------ ≅ 1 + x + 2x^2 (1 + x × μy (1 + x × y^2)^2)^2        +  x(x × (1 + x × μy (1 + x × y^2)^2)^2)^2
+------ ≅ 1 + x + 2x^2 (1 + x × μy (1 + x × y^2)^2)^2        +  x(x × (1 + x × μy (1 + x × y^2)^2)^2)^2
+------ ≅ 1 + x + 2x^2 (1 + 2x × μy (1 + x × y^2)^2 + (x × μy (1 + x × y^2)^2)^2)  +  x(x × (1 + x × μy (1 + x × y^2)^2)^2)^2
+------ ≅ 1 + x + 2x^2 + 4x^3 × μy (1 + x × y^2)^2  +  2x^2 (x × μy (1 + x × y^2)^2)^2)  +  x(x × (1 + x × μy (1 + x × y^2)^2)^2)^2
+------ ≅ Σ_i,j∈N ix^j
+------ 
+------ 
+------ ≅ μz( (Σ_i,j∈N ix^j)^2  +  (2x × Σ_i,j∈N ix^j) × z)
+------ summan oikeasta alkiosta löytyy sarjat joissa x:n kerroin on kahdella jaollinen ja nolla exponenttinen alkio puuttuu.
+------ ≅ μz( (Σ_i,j∈N ix^j)^2  +  (Σ_i,j∈N i2x^(j+1)) × z)
+------ -- koska vasen summan osa korotettuna toiseen sisälttää identiteetti alkion ja kaikki exponentista aiheutuvat arvot ovat jo loputtomaan joukkoon kuuluvaa muotoa.
+------ 
+------ ≅ μz( (Σ_i,j∈N ix^j)  +  2(Σ_i,j∈N ix^(j+1)) × z)
+------ ≅ μz( (Σ_i,j∈N ix^0)  +  (Σ_i,j∈N ix^(j+1))  +  2(Σ_i,j∈N ix^(j+1)) × z)
+------ ≅ μz( (Σ_i∈N i) + (Σ_i,j∈N ix^(j+1))  +  2(Σ_i,j∈N ix^(j+1)) × z)
+------ ≅ μz( (Σ_i∈N i) + (Σ_i,j∈N ix^(j+1)) × (1 + 2 × z))
+------ 
+------ Olen hukassa.
+------ 
+------ tavoite: Σ_n∈N (2 + Σ_i,j∈N ix^j)^n
+------ 
+------ 
+------ Onnettomia yrityksiä:
+------ 
+------ ≅ μz( μy(1 + x × y^2)^2)  +  (2x × (1 + x × μy(1 + x × y^2)^2)) × z)
+------ ≅ μz( μy(1 + x × y^2)^2)  +  (2x + 2x^2 × μy(1 + x × y^2)^2) × z)
+------ ≅ μz( μy(1 + x × y^2)^2)  +  (2xz + 2x^2z × μy(1 + x × y^2)^2 × z))
+------ 
+------ 
+------ ≅ μz( (1 + x × μy(1 + x × y^2))^2                            +  (2xz + 2x^2z × μy(1 + x × y^2)^2 × z))
+------ ≅ μz( (1 + x × μy(1 + x × y^2)) × (1 + x × μy(1 + x × y^2))  +  (2xz + 2x^2z × μy(1 + x × y^2)^2 × z))
+------ ≅ μz( 1 + 2x × μy(1 + x × y^2)  +  (x × μy(1 + x × y^2))^2   +  (2xz + 2x^2z × μy(1 + x × y^2)^2 × z))
+------ ≅ μz( 1 + 2x × μy(1 + x × y^2)  +  (x × μy(1 + x × y^2))^2   +  (2xz + 2x^2z × μy(1 + x × y^2)^2 × z))
+------ 
+------ 
+------ ≅ μy(1 + x × y^2)^2  +  2x × μy(1 + x × y^2) × μz( μy(1 + x × y^2)^2)  +  (2x × μy(1 + x × y^2)) × z)
+------ ≅ (1 + x × μy(1 + x × y^2))^2                              +  2x × μy(1 + x × y^2) × μz( μy(1 + x × y^2)^2)  +  (2x × μy(1 + x × y^2)) × z)
+------ ≅ (1 + x × μy(1 + x × y^2)) × (1 + x × μy(1 + x × y^2))    +  2x × μy(1 + x × y^2) × μz( μy(1 + x × y^2)^2)  +  (2x × μy(1 + x × y^2)) × z)
+------ ≅ 1 + 4x × μy(1 + x × y^2) + x^2 + μy(1 + x × y^2)^2   +  2x × μy(1 + x × y^2) × μz( μy(1 + x × y^2)^2)  +  (2x × μy(1 + x × y^2)) × z)
+------ 
+------ 
+------ ≅ μy(1 + x × y^2)^2  +  2x × μy(1 + x × y^2) × ((μy(1 + x × y^2)^2)  +  (2x × μy(1 + x × y^2)) × μz(( μy(1 + x × y^2)^2)  +  (2x × μy(1 + x × y^2)) × z))
+------ ≅ μy(1 + x × y^2)^2  +  2x × μy(1 + x × y^2) × ((μy(1 + x × y^2)^2)  +  (2x × μy(1 + x × y^2)) × μz(( μy(1 + x × y^2)^2)  +  (2x × μy(1 + x × y^2)) × z)
+------ 
+------ 2 + btree
+------ 
+------ ≅ μy.F^2  +  2x × (μy.F^3)  +  2x × μy.F × (2x × μy.F) × μz((μy.F^2)  +  (2x × μy.F) × z))
+------ ≅ μy.F^2  +  2x × (μy.F^3)  + 4x^2 × μy.F^4  +  2x × μy.F × (2x × μy.F) ×  (2x × μy.F) × μz((μy.F^2)  +  (2x × μy.F) × z)
+------ 
+------ tavoite:
+------ 
+------ ≅ (μz(1  +  2 × z  +  μy (1 + x × y^2) × z))
+------ ≅ (μz(1 + (2 + μy (1 + x × y^2)) × z))
+------ ≅ [2 + btree]
+------ 
+------ Σ_n∈N (2 + Σ_i,j∈N ix^j)^n
+
+------ Ei minulla ole hajuakaan miten tämä pitäisi saavuttaa.
+------ answer: (a, [(Bool, btree)])
 
 -}
--- src/Week8/Exercise2.hs
+++ src/Week8/Exercise2.hs
@@ -1,5 +1,6 @@
-{-# OPTIONS_GHC -XDeriveDataTypeable  #-}
-{-# OPTIONS_GHC -fwarn-incomplete-patterns  #-}
+
+{-# OPTIONS_GHC -XDeriveDataTypeable #-}
+{-# OPTIONS_GHC -fwarn-incomplete-patterns #-}
 {-# OPTIONS_GHC -fwarn-hi-shadowing #-}
 {-# OPTIONS_GHC -fwarn-name-shadowing #-}
 
@@ -14,7 +15,7 @@ import Week8.Exercise1
 import Data.Bifunctor.Join
 import Data.Typeable
 
-data ZipperEnd = ZippersDeadEnd
+data ZipperEnd = ZippersDeadEnd | EmptyZipper | ShouldNeverHappen
      deriving (Show, Typeable)
 
 instance Exception ZipperEnd
@@ -24,9 +25,9 @@ instance Exception ZipperEnd
 --context from derivative := 1
 --type ZA := (a, ())
 
-stepperMaybe :: MonadThrow m => Maybe a -> m (a, ())
-stepperMaybe (Just a) = pure (a,())
-stepperMaybe (Nothing) = throwM ZippersDeadEnd
+zipperMaybe :: MonadThrow m => Maybe a -> m (a, ())
+zipperMaybe (Just a) = pure (a,())
+zipperMaybe (Nothing) = throwM EmptyZipper
 
 stepMaybe :: MonadThrow m => (a, ()) -> m (a, ())
 stepMaybe a = pure a
@@ -46,8 +47,8 @@ type ZA (a, (Bool, a))
 
 -}
 
-stepperJoin :: MonadThrow m => Join (,) a -> m (a, (Bool, a))
-stepperJoin jaa = pure (fst (runJoin jaa), (True, snd $ runJoin jaa ))
+zipperJoin :: MonadThrow m => Join (,) a -> m (a, (Bool, a))
+zipperJoin jaa = pure (fst (runJoin jaa), (True, snd $ runJoin jaa ))
 
 stepJoin :: MonadThrow m => (a, (Bool, a)) -> m (a, (Bool, a))
 stepJoin (right, (False, other)) =  pure (other, (True, right))
@@ -59,33 +60,89 @@ context := [a]×[a]
 ZA := (a,[a]×[a])
 -}
 
-stepperList :: MonadThrow m => [a] -> m (a,([a], [a]))
-stepperList (x:xs) = pure (x, ([],xs))
-stepperList [] = throwM ZippersDeadEnd
+zipperList :: MonadThrow m => [a] -> m (a,([a], [a]))
+zipperList (x:xs) = pure (x, ([],xs))
+zipperList [] = throwM EmptyZipper
 
-stepList :: MonadThrow m => (a,([a], [a])) -> m (a,([a],[a]))
-stepList (a,(as, b:bs)) = pure (b,(a:as,bs))
-stepList (_,(_, [])) = throwM ZippersDeadEnd
+stepListForward :: MonadThrow m => (a,([a], [a])) -> m (a,([a],[a]))
+stepListForward (a,(as, b:bs)) = pure (b,(a:as,bs))
+stepListForward (_,(_, [])) = throwM ZippersDeadEnd
 
-stepperStream :: MonadThrow m => Stream a -> m (a, ([a],Stream a))
-stepperStream (a :> as) = pure (a, ([],as))
-
-stepStream :: MonadThrow m => (a, ([a],Stream a)) -> m (a, ([a],Stream a))
-stepStream (a, (as, b :> bs))= pure (b, (a:as, bs))
-
-{-
-()
-D(μy.F) ↦ μz.(((D_x F) |y=μy.F) + ((D_y F) |y=μy.F) × z)
-D(F|y=S) ↦ D_x(F|y=S) + D_y(F|y=S) × D_x S
+stepListReverse :: MonadThrow m => (a,([a], [a])) -> m (a,([a],[a]))
+stepListReverse (b,(a:as, bs)) = pure (a,(as, b:bs))
+stepListReverse (_,([], _)) = throwM ZippersDeadEnd
 
 
-A := Leaf | Node (Tree a) a (Tree a)
-μy (1 + (y×y×a))
-(1 + (ay^2))
-D >> y^2
-(1 + (y×y×a)) × (1 + (y×y×a))
-context 
--}
 
---stepBTree :: MonadThrow m => Binary.Tree a -> m (a, ([a],Stream a))
---stpeBTree = undefined
+zipperStream :: MonadThrow m => Stream a -> m (a, ([a],Stream a))
+zipperStream (a :> as) = pure (a, ([],as))
+
+stepStreamForward :: MonadThrow m => (a, ([a],Stream a)) -> m (a, ([a],Stream a))
+stepStreamForward (a, (as, b :> bs))= pure (b, (a:as, bs))
+
+stepStreamReverse :: MonadThrow m => (a, ([a],Stream a)) -> m (a, ([a],Stream a))
+stepStreamReverse (b, (a:as, bs))= pure (a, (as, b :> bs))
+stepStreamReverse (b, ([], bs))= throwM ZippersDeadEnd
+
+
+
+zipperBTree :: MonadThrow m => Binary.Tree a -> m (a, BTC a)
+zipperBTree Binary.Leaf = throwM EmptyZipper
+zipperBTree (Binary.Node (left) a (right)) = pure $ (a, BTCEnd left right) 
+
+stepBTreeLeft :: MonadThrow m => (a, BTC a) -> m (a, BTC a)
+stepBTreeLeft (b, BTCEnd (Binary.Node l a r) other) = pure (a, (L, b, other) :-< (BTCEnd l r))
+stepBTreeLeft (b, (history :-< next)) = stepBTreeLeft (b, next) >>= (\out -> pure (fst out, history :-< snd out))
+stepBTreeLeft (b, BTCEnd Binary.Leaf _) = throwM ZippersDeadEnd
+-- This list like data structure could be reversed for efficiency? I think I maybe did something wrong in relation to recursion with the derivative.
+
+stepBTreeRight :: MonadThrow m => (a, BTC a) -> m (a, BTC a)
+stepBTreeRight (b, BTCEnd other (Binary.Node l a r)) = pure (a, (R, b, other) :-< (BTCEnd l r))
+stepBTreeRight (b, (history :-< next)) = stepBTreeRight (b, next) >>= (\out -> pure (fst out, history :-< snd out))
+stepBTreeRight (b, BTCEnd _ Binary.Leaf) = throwM ZippersDeadEnd
+
+stepBTreeUp :: MonadThrow m => (a, BTC a) -> m (a, BTC a)
+stepBTreeUp (a, BTCEnd _ _) = throwM ZippersDeadEnd
+stepBTreeUp (a, (branch, b, other) :-< BTCEnd l r) = case branch of
+  L -> pure (b, BTCEnd (Binary.Node l a r ) other)
+  R -> pure (b, BTCEnd other (Binary.Node l a r )) 
+stepBTreeUp (a, (history :-< next)) =  stepBTreeUp (a, next) >>= (\out -> pure (fst out, history :-< snd out))
+
+
+
+
+zipperRTree :: MonadThrow m => Rose.Tree a -> m (RoseZipper a)
+zipperRTree (Rose.Node a (ts)) = pure (a, (RTLZ ts))
+
+rollForwardRTree :: MonadThrow m => RoseZipper a -> m (RoseZipper a)
+rollForwardRTree (a, RTLZ []) =  throwM ZippersDeadEnd
+rollForwardRTree (a, RTLZ (t:ts)) = pure (a, RTLZ (t:ts))
+rollForwardRTree (aOld, host :* RTLZ l) = let
+  f h = case h of
+    (Rose.Node b _ :-: LRZ ((Rose.Node aNew ts:ts'))) ->
+      (aNew, ((Rose.Node aOld l :-: (Rose.Node b [] :-: LRZ ts'))) :* RTLZ ts)
+    (first :-: second) -> let
+      out = (f h)
+      a :* b = snd out
+      in
+      (fst out , (first :-: a) :* b)
+    --  (LRZ tst) -> undefined -- should never occure -- this should be done differently. -
+  in pure $ f host
+  
+rollForwardRTree (b, first :* second) = rollForwardRTree (b,second) >>= (\out -> pure $ (fst out, first :* snd out))
+
+rollbackwardRTree :: MonadThrow m => RoseZipper a -> m (RoseZipper a)
+rollbackwardRTree (a, (RTLZ (t:ts))) = pure (a, RTLZ (t:ts))
+rollbackwardRTree _ = undefined -- more or less the same as rollForwards with bugs removed.
+
+
+goDownRTree :: MonadThrow m => RoseZipper a -> m (RoseZipper a)
+goDownRTree (_, RTLZ []) = throwM ZippersDeadEnd
+goDownRTree (b, RTLZ ((Rose.Node a (ts)):ts')) = 
+  pure (a, ((Rose.Node b [] :-: LRZ ts) :* RTLZ ts))
+goDownRTree (b, first :* second) = goDownRTree (b, second) >>= (\out -> pure $ (fst out, first :* snd out))
+
+goUpRTree :: MonadThrow m => RoseZipper a -> m (RoseZipper a)
+goUpRTree = undefined
+
+
